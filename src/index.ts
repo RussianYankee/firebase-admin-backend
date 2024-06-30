cab932cd85b03b0b667ed11e33152cd25c42dc70
Anton Evlampeev <48838700+RussianYankee@users.noreply.github.com>
2024-06-30 17:17:16 -0500

    admin emulator

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,31 @@
 import express from 'express'
 import admin, { ServiceAccount } from 'firebase-admin'
 import serviceAccount from '../firebase-service-acc.json'
-import {connectAuthEmulator, getAuth} from "firebase/auth"
-import { initializeApp } from 'firebase/app'
 
 const PORT = 3000
 const app = express()
 
 app.use(express.json())
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBlwawhaQcMq679gWfzDZTB4A1TasmoJxc",
-    authDomain: "sample-web-app-547a0.firebaseapp.com",
-    projectId: "sample-web-app-547a0",
-    storageBucket: "sample-web-app-547a0.appspot.com",
-    messagingSenderId: "287969019433",
-    appId: "1:287969019433:web:11fe0c2c0f6ab38cc03e38"
-};
-
-const firebaseApp = initializeApp(firebaseConfig)
-const auth = getAuth(firebaseApp)
+process.env['FIREBASE_AUTH_EMULATOR_HOST'] =  '127.0.0.1:9099'
 
 const fbAdminApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount)
 });
 
-connectAuthEmulator(auth, 'http://localhost:9099')
+//REST
+app.post('/signup', async (req, res) => {
+  console.log(`[SERVER]: Fetching user... ${req.body.email}`)
+  const user = await fbAdminApp.auth().getUserByEmail(req.body.email)
+  if (user) {
+    console.log(`[UID VALIDATION]: ${req.body.uid == user.uid}`)
+    await fbAdminApp.auth().setCustomUserClaims(user.uid, {
+      "owner": true
+    })
+    res.send("SUCCESS")
+  } else {
+    res.send("ERROR: user not found")
+  }
+})
 
 app.listen(PORT, () => console.log(`[SERVER]: started listening to port ${PORT}`))
